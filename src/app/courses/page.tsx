5832c1d863d5061b5ee736b60e4695548f866d3f
Saleks <saleks@example.com>
2026-07-23 09:39:26 +0300

    Use only active addresses in courses

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -113,6 +113,9 @@ export default async function CoursesPage() {
     }),
 
     prisma.address.findMany({
+      where: {
+        isActive: true,
+      },
       select: {
         id: true,
         name: true,
